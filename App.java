ac826069eaf388632881777d1d29905529ddec61
Rouzen0 <98684885+Rouzen0@users.noreply.github.com>
2022-02-07 15:19:28 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+/**
+ *
+ * @author 1936447
+ */
+public class App{
+  public static void main(String[]args){
+    Order o1 = new Order();
+    Shipment sh1 = new Shipment("Ahmed", 
+        "248 NE. Pleasant St. Niceville, FL 32578",
+        "123-477-0001");
+  }
+}
+
